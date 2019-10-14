37fcc6e1889fa9f2d9c0cf990d06c6b8465d77ce
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-13 21:20:55 -0300

    new: add components

---
+++ src/components/Menu/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import QRcode from 'react-native-qrcode';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Container, Code, Nav, NavItem, NavText} from './styles';
+
+export default function Menu() {
+  return (
+    <Container>
+        <Code>
+            <QRcode 
+                value="https://github.com/HugoHoffmann"
+                size={80}
+                fgColor="#fff"
+                bgColor="#8b10ae"
+            />
+        </Code>
+        <Nav>
+            <NavItem>
+                <Icon name="help-outline" size={20} color="#fff" />
+                <NavText>Me ajuda</NavText>
+            </NavItem>
+            <NavItem>
+                <Icon name="person-outline" size={20} color="#fff" />
+                <NavText>Perfil</NavText>
+            </NavItem>
+            <NavItem>
+                <Icon name="credit-card" size={20} color="#fff" />
+                <NavText>Configurar Cartão</NavText>
+            </NavItem>
+            <NavItem>
+                <Icon name="smartphone" size={20} color="#fff" />
+                <NavText>Configurações do App</NavText>
+            </NavItem>
+        </Nav>
+    </Container>
+  );
+}
---
+++ src/components/Menu/styles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+import styled from 'styled-components/native';
+
+export const Container = styled.ScrollView`
+  margin: 0 30px;
+`;
+
+export const Code = styled.View`
+    background: #fff;
+    padding: 10px;
+    align-self: center;
+`;
+
+export const Nav = styled.View`
+    margin-top: 30px;
+    border-top-width: ${StyleSheet.hairlineWidth}px;
+    border-top-color: rgba(255, 255, 255, 0.8);
+`;
+export const NavItem = styled.View``;
+export const NavText = styled.Text``;
